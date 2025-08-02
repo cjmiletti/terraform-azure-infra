342b6024045d14cae7984b1a7c8fbf3f882702ef
infosecsniffer <cmiletti@opengatetech.com>
2025-08-02 11:50:57 -0400

    adding new variables

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -17,3 +17,24 @@ variable "tags" {
   type        = map(string)
   description = "Tags to apply to all resources"
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret"
+  sensitive   = true
+}
